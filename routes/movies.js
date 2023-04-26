6b95a2bdce3d0390b0098440f1358c424b67b933
Arantxa Martinez <Shayne.martinez23@gmail.com>
2023-04-26 17:10:13 -0400

    Iteration 10

--- routes/movies.js
+++ routes/movies.js
@@ -1,49 +1,93 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Celebrity= require('../models/Celebrity.model')
+const Celebrity = require("../models/Celebrity.model");
 
-const Movie = require('../models/Movie.model')
+const Movie = require("../models/Movie.model");
 
-router.get('/all-movies', (req, res, next) => {
-
-    Movie.find()
+router.get("/all-movies", (req, res, next) => {
+  Movie.find()
     .then((movies) => {
-        res.render('movies/movies.hbs', {movies})
+      res.render("movies/movies.hbs", { movies });
     })
-    .catch((err)=>{
-        console.log(err)
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/add-movies", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("movies/new-movie.hbs", { celebrities });
     })
-  });
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
+router.post("/add-movie", (req, res, next) => {
+  let { title, genre, plot, cast } = req.body;
 
-  router.get('/add-movies', (req, res, next) => {
-Celebrity.find()
-.then((celebrities) => {
-    res.render('movies/new-movie.hbs', {celebrities})
+  Movie.create({
+    title,
+    genre,
+    plot,
+    cast,
+  })
+    .then((createdeMovie) => {
+      console.log("Created movie", createdeMovie);
+      res.redirect("/movies/all-movies");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/movie-details/:id", (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .populate("cast")
+    .then((movie) => {
+      res.render("movies/movie-details.hbs", movie);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/delete/:id", (req, res, next) => {
+  const { id } = req.params;
+  Movie.findByIdAndDelete(id)
+    .then((movie) => {
+      res.redirect("/movies/all-movies");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/edit/:id", (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .populate("cast")
+    .then((movie) => {
+      res.render("movies/edit-movies.hbs", movie);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post("/edit/:id", (req, res, next) => {
+  const { id } = req.params;
+  const { title, genre, plot, cast } = req.body;
+  Movie.findByIdAndUpdate(id, req.body, { new: true })
+  .then((movie) => {
+    res.redirect(`/movies/movie-details/${id}`);
 })
 .catch((err) => {
-    console.log(err)
-})
-  });
-
-router.post('/add-movie',(req, res, next) =>{
-
-let {title, genre, plot, cast}= req.body
-
-    Movie.create({
-        title,
-        genre,
-        plot,
-        cast
-    })
-    .then((createdeMovie) =>{
-        console.log("Created movie", createdeMovie)
-        res.redirect('/movies/all-movies')
-    })
-    .catch((err) =>{
-        console.log(err)
-    })
-} )
+  console.log(err);
+});
+});
 
 module.exports = router;
